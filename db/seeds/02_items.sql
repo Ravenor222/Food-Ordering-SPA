9525ea444d1909356056f93f4d6b560741b095e2
Nick <ndanvers222@gmail.com>
2020-03-01 10:29:43 -0800

    name, db pictures, title

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -1,9 +1,9 @@
 INSERT INTO items (price, calories, name, picture)VALUES
-(9.99, 1999, 'DacBurger', '/images/burger-one.jpeg'),
-(19.99, 1888, 'DacTriple', '/images/cheese-burger.jpg'),
-(29.99, 1777, 'DacFries', '/images/fries.jpg'),
-(5.99, 698, 'DacShake', '/images/dacMonaldsMilkshake.png'),
-(13.99, 999, 'DacDouble', '/images/burger-two.jpg'),
-(14.99, 1299, 'DacNuggets', '/images/nuggets.jpg'),
+(39.99, 1999, 'Steak and Fries', '/images/steak.jpg'),
+(13.99, 1888, 'Poutine', '/images/poutine.jpg'),
+(19.99, 1400, 'Hamburger', '/images/cheese-burger.jpg'),
+(23.99,898, 'Salmon', '/images/salmon.jpeg'),
+(33.99,899, 'Halibut', '/images/halibut.jpg'),
+(9.99, 1299, 'Mousse Cake', '/images/mousse.jpg'),
 (1.99, 299, 'Soda', '/images/soda.jpg');
 -- ();
